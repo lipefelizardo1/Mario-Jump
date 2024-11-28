c4c36079fabb5277f0ed7764a086e415f1a9eba6
lipefelizardo <92354237+lipefelizardo1@users.noreply.github.com>
2024-11-28 10:26:30 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let score = 0; 
+const scoreElement = document.querySelector('.score'); 
+
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
@@ -5,30 +8,19 @@ const jump = () => {
     mario.classList.add('jump');
 
     setTimeout(() => {
-
         mario.classList.remove('jump');
-
     }, 500);
-}
-
-let score = 0;
-const scoreElement = document.querySelector('.score');
+};
 
 const loop = setInterval(() => {
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const pipePosition = pipe.offsetLeft; 
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); 
 
-    if (pipePosition > 0 && marioPosition >= 80) {
-        score++; // Aumenta a pontuação
-        scoreElement.textContent = `Pontuação: ${score}`; 
-    }
-    
+    // Verifica se o Mario colidiu com o cano
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
-        
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
@@ -36,19 +28,22 @@ const loop = setInterval(() => {
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
 
-
         clearInterval(loop);
-        
+
+    
         alert(`Fim de jogo! Sua pontuação foi: ${score}`);
-    } 
+    }
 
-    if (pipePosition < 0 && pipePosition > -80) {
-        if (marioPosition > 80) { 
-            score++; // Aumenta a pontuação
-            scoreElement.textContent = `Pontuação: ${score}`; 
-        }
+    if (pipePosition < 0 && marioPosition > 80 && !pipe.classList.contains('scored')) {
+        score++; // Incrementa a pontuação
+        scoreElement.textContent = `Pontuação: ${score}`; 
+        pipe.classList.add('scored'); 
+    }
 
+
+    if (pipePosition < -80) {
+        pipe.classList.remove('scored');
+    }
 }, 10);
-
 
 document.addEventListener('keydown', jump);
